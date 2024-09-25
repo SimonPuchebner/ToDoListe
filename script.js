bd0d1fd674b9adbd2404c61a97d81a97a648564d
Grurl Stefan <stefan.grurl@htlwy.at>
2024-09-25 10:41:13 +0200

    Localstorage started and completed

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
-function add(){
+window.onload = function() {
+    loadItems();
+};
+function add() {
     let div = document.createElement("div");
-    div.style.backgroundColor= "red";
-    div.setAttribute("height","50px");
-    div.setAttribute("width","50px");
-   
+    div.style.backgroundColor = "red";
+    div.setAttribute("height", "50px");
+    div.setAttribute("width", "50px");
     let button = document.createElement("button");
-    button.setAttribute("class","button");
-    button.addEventListener('click', function(){
-        div.style.backgroundColor= "green";
-    })
+    button.setAttribute("class", "button");
+    button.addEventListener('click', function() {
+        div.style.backgroundColor = "green";
+        saveItems(); 
+    });
     button.textContent = "Fertig";
- 
+
     let text = document.createElement("div");
     text.innerHTML = document.getElementById("text").value;
     text.style.backgroundColor = "white";
- 
+
     div.appendChild(text);
-    div.appendChild(button);    
-    console.log(div)
-   
+    div.appendChild(button);
+
     let append = document.querySelector('#items');
     append.appendChild(div);
+
+    saveItems();
+}
+function saveItems() {
+    let items = document.querySelectorAll('#items > div');
+    let itemList = [];
+
+    items.forEach((item) => {
+        let content = item.firstChild.innerHTML; 
+        let color = item.style.backgroundColor; 
+        itemList.push({ content: content, color: color });
+    });
+    localStorage.setItem('toDoItems', JSON.stringify(itemList));
+}
+
+function loadItems() {
+    let savedItems = localStorage.getItem('toDoItems');
+
+    if (savedItems) {
+        let itemList = JSON.parse(savedItems);
+        let append = document.querySelector('#items');
+
+        itemList.forEach((itemData) => {
+            let div = document.createElement("div");
+            div.style.backgroundColor = itemData.color || "red"; 
+            div.setAttribute("height", "50px");
+            div.setAttribute("width", "50px");
+
+            let button = document.createElement("button");
+            button.setAttribute("class", "button");
+            button.addEventListener('click', function() {
+                div.style.backgroundColor = "green";
+                saveItems();
+            });
+            button.textContent = "Fertig";
+
+            let text = document.createElement("div");
+            text.innerHTML = itemData.content;
+            text.style.backgroundColor = "white";
+
+            div.appendChild(text);
+            div.appendChild(button);
+            append.appendChild(div);
+        });
+    }
 }
