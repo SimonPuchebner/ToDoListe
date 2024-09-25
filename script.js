96e244c79cc40dedfd252db43e719a5b7aa0f096
Jonas G <jonas.grestenberger@htlwy.at>
2024-09-25 09:32:59 +0200

    js function add version 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function add(){
+    let div = document.createElement("div");
+    div.style.backgroundColor= "lightgrey";
 
+    let input = document.getElementById("text").textContent;
+    input.setAttribute("class","textInBox");
+    
+    let checkb = document.createElement("input");
+    checkb.setAttribute("type","checkbox");
+
+
+    let button = document.createElement("button");
+    button.setAttribute("class","button");
+    button.addEventListener('click', function(){
+        input.setAttribute("class","delText");
+        div.style.backgroundColor= "darkgrey";
+    })
+    button.textContent = "Fertig";
+
+    div.appendChild(checkb);
+    div.appendChild(input);
+    div.appendChild(button);    
+    
+}
